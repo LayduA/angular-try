e366462cea30a83af4a30bc7e4bd539936ca8452
LayduA <adrien.laydu@gmail.com>
2020-03-27 19:41:20 +0100

    3 files modified, 1 added

---
+++ src/app/film-infos.ts
@@ -0,0 +1,20 @@
+export const filminfos = [
+  {
+    number_f:1,
+    title: 'Catch me if you can',
+    genre : 'Crime',
+    duration: 54.68
+  },
+  {
+    number_f:2,
+    title: 'Drive',
+    genre : 'Crime',
+    duration: 54.68
+  },
+  {
+    number_f:3,
+    title: 'Twilight',
+    genre : 'Romance',
+    duration: 54.68
+  }
+];
--- src/app/list-films/list-films.component.ts
+++ src/app/list-films/list-films.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { films } from '../films';
+import { filminfos } from '../film-infos';
 
 @Component({
   selector: 'app-list-films',
@@ -10,9 +11,20 @@ export class ListFilmsComponent implements OnInit {
 
   film_list = films;
   film_searched = '';
+  film_infos = filminfos;
+  film_toshow = null;
 
   search(filmTitle : string){
     this.film_searched = filmTitle;
+    this.show_info(filmTitle);
+  }
+
+  show_info(filmTitle : string){
+    for(var i:number = 0; i < this.film_infos.length; i++){
+      if(this.film_infos[i].title === filmTitle){
+        this.film_toshow = this.film_infos[i]
+      }
+    }
   }
   constructor() { }
 
